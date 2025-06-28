992a7c7c90567cad1fd68fba703c16c75d69a69c
PStars03 <petrixyoga@gmail.com>
2025-06-28 20:45:46 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>;
+
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
